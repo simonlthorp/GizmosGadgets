b69880218d6d25fa1e11aac47f955d08244085c5
Jessica <jpointon@outlook.com>
2017-11-15 12:34:12 -0800

    menu nav and gadget resize

---
+++ Assets/Scripts/sceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class sceneLoader : MonoBehaviour {
+
+    public void LoadTheLevel(string theLevel) {
+        SceneManager.LoadScene(theLevel);
+    }
+
+    public void QuitTheGame() {
+        Application.Quit();
+    }
+
+}
