4750851956810abf42b0a2fe67366768d07eba4b
Unknown <mariazhang91@gmail.com>
2017-11-16 05:25:32 -0800

    boot animation and part  of switch animation(no code)

---
+++ Assets/bootAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bootAnimation : MonoBehaviour {
+    Animator ani;
+	// Use this for initialization
+	void Start () {
+        ani = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ani.SetBool("bootAnimation", false);
+            
+        }
+	}
+}
