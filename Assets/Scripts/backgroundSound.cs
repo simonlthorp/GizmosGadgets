52631ea272498a95bfbc6e11a10b63fbc2ae819a
Unknown <mariazhang91@gmail.com>
2017-11-18 00:55:24 -0800

    sound update all

---
+++ Assets/Scripts/backgroundSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundSound : MonoBehaviour {
+    AudioSource audioSource;
+    // Use this for initialization
+    void Start () {
+
+    audioSource = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (audioSource.isPlaying)
+        {
+            if (Input.GetAxisRaw("Pause") !=0 )
+            {
+                audioSource.Pause();
+
+            }
+        }
+        else if (!audioSource.isPlaying)
+        {
+            if (Input.GetAxis("Pause") != 0)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
