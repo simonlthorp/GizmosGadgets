a776572eb9b91a4eff516dcbb36eb3c1af821c16
Unknown <mariazhang91@gmail.com>
2017-11-19 16:19:11 -0800

    boot & swirch animation(not resize the collision box yet)

---
+++ Assets/Scripts/bootAnimation.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bootAnimation : MonoBehaviour {
+    //Animator ani;
+    public Animator anim;
+    bool act = false;
+    bool inTrigger;
+
+    void Start () {
+
+        //anim = GetComponent<Animator>();
+      //  GameObject boot= GameObject.Find("BootSwitch");
+       // anim = boot.GetComponent<Animator>();
+       
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    Debug.Log("Animation");
+        //    anim.Play("bootAnimation");
+
+        //}
+        //if (Input.GetAxis("Fire1") != 0 && inTrigger)
+        //{
+        //    if (Input.GetAxisRaw("Fire1") != 0)
+        //    {
+
+        //        globalVars.switchFlippedD = true;
+        //        Debug.Log("aaaa");
+        //              ani.Play("bootAnimation");
+        //    }
+        //}
+
+        if (Input.GetAxisRaw("Fire1") != 0 && inTrigger)
+        {
+
+            Debug.Log("aaaa");
+            anim.Play("bootAnimation");
+
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "torso")
+        {
+            inTrigger = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "torso")
+        {
+            inTrigger = false;
+        }
+    }
+
+
+
+}
--- Assets/Scripts/switchAnimation.cs
+++ Assets/Scripts/switchAnimation.cs
@@ -2,22 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class soundEffect : MonoBehaviour {
-
-    AudioSource audioSource;
+public class switchAnimation : MonoBehaviour {
+    public Animator anim;
+    bool act = false;
     bool inTrigger;
+
+    
     // Use this for initialization
     void Start () {
-        audioSource = GetComponent<AudioSource>();
+       // anim = GetComponent<Animator>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+    public void DoSomething()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetAxisRaw("Fire1") != 0 && inTrigger)
         {
-            audioSource.Play();
+            
+            anim.Play("switchAnimation");
 
         }
+        //if (Input.GetKeyDown(KeyCode.E)) //&& inTrigger)
+        //{
+
+        //    Debug.Log("aaaa");
+        //    anim.Play("SwitchAnimation");
+
+        //}
     }
     void OnTriggerEnter(Collider other)
     {
